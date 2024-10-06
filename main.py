4ea4b7531874fb85375d51e48ebb47a537e52144
toantran980 <jokemaking90@gmail.com>
2024-10-05 18:39:35 -0700

    change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from handle_movements import Handle
+from tic_tac_toe import Battle
 # from load_images import Image
-#import tic_tac_toe
 
 class Game:
     def __init__(self) -> None:
@@ -10,8 +10,10 @@ class Game:
         self.clock = pygame.time.Clock()
         self.handle = Handle(500, 650)
         self.map = pygame.image.load('Graphics/map1.png')
-        # Variable that stores whether we are in a battle or not
-        self.battle = None
+        self.BOARD = pygame.image.load("Graphics/Board.png")
+        self.X_IMG = pygame.image.load("Graphics/X.png")
+        self.O_IMG = pygame.image.load("Graphics/O.png")
+        self.FONT = pygame.font.Font("Font/Pixeltype.ttf")
 
     def run(self) -> None:
         is_running = True
@@ -24,6 +26,9 @@ class Game:
                         is_running = False
                         pygame.quit()
                         sys.exit()
+                    if event.key == pygame.K_b:
+                        self.battle = Battle(self.screen, self.BOARD, self.X_IMG, self.O_IMG, self.FONT)
+
 
             self.screen.blit(self.map, (0, 0))
             self.handle.handle_mov(event)
@@ -34,9 +39,9 @@ class Game:
             self.clock.tick(60)
     
 
-    def load_assets(self):
-        BOARD = pygame.imge.load("Graphics/Board.png").convert_alpha()
-        FONT = pygame.font.Font("Font/Pixeltype.ttf", 100)
+    #def load_assets(self):
+        #BOARD = pygame.imge.load("Graphics/Board.png").convert_alpha()
+        #FONT = pygame.font.Font("Font/Pixeltype.ttf", 100)
 
 if __name__ == '__main__':
     game = Game()
